f69fd7359f3698333e8d488cd3b56774c06dc529
Orthant Bing <orthantbing@live.com>
2017-05-03 22:00:03 +0530

    images and fileloader dependencies

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [ 
         CommonModule,
-        BrowserModule
+        BrowserModule,
+        NgbModule.forRoot()
     ],
     exports: [],
     providers: [],
